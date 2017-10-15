8cad7ccc92ae68521aaef07f70f810314f29e08e
Sakib Hasan <ajobbalok22@gmail.com>
2017-10-15 15:51:46 +0600

    optimized for larger file

--- main.go
+++ main.go
@@ -8,16 +8,23 @@ import (
 	"sync/atomic"
 	"time"
 )
-type ExampleTask string
+
+type ExampleTask []byte
+
 var readOps int32 = 0
 
 func (e ExampleTask) Execute() {
-	parts := strings.Count(string(e), " ")
-	atomic.AddInt32(&readOps, int32(parts+1))
+	line := string(e)
+	line = strings.TrimSpace(line)
+	spaces := strings.Count(line, " ")
+	line_breaks := strings.Count(line, "\n")
+	atomic.AddInt32(&readOps, int32(spaces)+1+int32(line_breaks))
 }
 
 func main() {
+
 	start := time.Now()
+
 	file, err := os.Open("wap.txt")
 
 	if err != nil {
@@ -27,29 +34,21 @@ func main() {
 
 	defer file.Close()
 
-	info, _ := file.Stat()
-
-	// calculate the bytes size
-	var size int64 = info.Size()
-	bytes := make([]byte, size)
-	byte_size := int(info.Size())
 	buffer := bufio.NewReader(file)
-	_, err = buffer.Read(bytes)
 
-	pool := NewPool(10)
-	for i := 0; i < byte_size; i += 90000 {
-		ba := bytes[i:min(i+90000, byte_size)]
-		pool.Exec(ExampleTask(ba))
+	bytes := make([]byte, 100000)
+	pool := NewPool(1000)
+	for {
+		v, _ := buffer.Read(bytes)
+		if v != 0 {
+			pool.Exec(ExampleTask(bytes))
+		} else {
+			break
+		}
 	}
+
 	pool.Close()
 	pool.Wait()
 	fmt.Println(readOps)
 	fmt.Println(time.Since(start))
-}
-
-func min(a, b int) int {
-	if a <= b {
-		return a
-	}
-	return b
 }
